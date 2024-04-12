1b35b29b355457e8e5bd85dae588eaf69023c021
Elena <SOP92117@educastur.es>
2024-04-12 16:23:31 +0200

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/ciber07/calculator07/Calculator07Application.java
@@ -0,0 +1,13 @@
+package com.ciber07.calculator07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator07Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciber07/calculator07/Calculator07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber07.calculator07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
