bc98f9f52d914b2c58e076a1a251dd7bd5cb880c
Elena <SOP92117@educastur.es>
2024-04-18 17:10:30 +0200

    Se añaden suma, controlador y test unitario

---
+++ src/main/java/com/ciber07/calculator07/Calculator.java
@@ -0,0 +1,9 @@
+package com.ciber07.calculator07;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    int sum(int a, int b) {
+        return a + b;
+    }
+}
--- src/main/java/com/ciber07/calculator07/Calculator07Application.java
+++ src/main/java/com/ciber07/calculator07/Calculator07Application.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Calculator07Application {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		SpringApplication.run(Calculator07Application.class, args);
 	}
-
 }
